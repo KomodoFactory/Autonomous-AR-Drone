4d525bb6ae0c6646f6f3459c9dfb6c3647a73635
KomodoFactory <github@komodofactory.com>
2017-03-30 16:25:37 +0200

    Done writing shit!

--- Node/deliveryDrone/serverTracking.js
+++ Node/deliveryDrone/serverTracking.js
@@ -5,7 +5,7 @@ var express = require('express')
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/indexpng.html');
+    res.sendfile(__dirname + '/public/indexForTrackingTest.html');
 });
 
 server.listen(8080);
